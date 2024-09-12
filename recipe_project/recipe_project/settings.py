f8fd87d6edafe2cfde524e22139be4b20e363c88
Katie Ho <kthodesign@gmail.com>
2024-09-11 20:20:25 -0700

    updated

--- recipe_project/recipe_project/settings.py
+++ recipe_project/recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = [https://cf-python-base.onrender.com]
+ALLOWED_HOSTS = ['cf-python-base.onrender.com']
 
 
 # Application definition
