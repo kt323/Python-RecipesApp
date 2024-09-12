5ddae3c8f3ff85dffcfdfd9c416391e0aeb0bcee
Katie Ho <kthodesign@gmail.com>
2024-09-11 20:31:50 -0700

    2.8

--- recipe_project/recipe_project/wsgi.py
+++ recipe_project/recipe_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+print("Loading WSGI application...")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
 
 application = get_wsgi_application()
